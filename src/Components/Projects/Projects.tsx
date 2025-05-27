8e52ce69b243c6454d2b0b1e59fdc18620291779
dimssu <aryansi1126@gmail.com>
2025-05-27 14:41:01 +0530

    View all projects UX fix

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from 'react-router-dom';
 import styles from './Projects.module.scss';
 import ProjectsViewer from './ProjectsViewer/ProjectsViewer';
+import { FaArrowUp } from 'react-icons/fa';
 
 const Projects = () => {
 
@@ -8,16 +9,18 @@ const Projects = () => {
 
   return (
     <div className={styles.ProjectsContainer}>
-      <div className={styles.ProjectsHeader}>
-        <div className={styles.ProjectsTitle}>
-          Projects
+      <div className={styles.ProjectWrapper}>
+        <div className={styles.ProjectsHeader}>
+          <button className={styles.ExpandButton} onClick={() => setSearchParams({ showProjects: 'true', hideNavbar: 'true' })}>
+            View All <span style={{ fontSize: '1.2rem', paddingLeft: '0.5rem' }}><FaArrowUp /></span>
+          </button>
+          <div className={styles.ProjectsTitle}>
+            Projects
+          </div>
         </div>
-        <button className={styles.ExpandButton} onClick={() => setSearchParams({ showProjects: 'true', hideNavbar: 'true' })}>
-          View More
-        </button>
-      </div>
-      <div className={styles.Projects}>
-        <ProjectsViewer />
+        <div className={styles.Projects}>
+          <ProjectsViewer />
+        </div>
       </div>
     </div>
   )
