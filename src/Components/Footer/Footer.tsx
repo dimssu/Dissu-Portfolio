49d840fee5ba212ac828a087d2de7a06f07c2321
dimssu <aryansi1126@gmail.com>
2025-06-03 22:44:16 +0530

    Refactor Footer component to use a constant for copyright text for improved maintainability

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import styles from './Footer.module.scss'
 
 const Footer = () => {
+  const footerContent = {
+    copyright: '© 2025 Aryan Singh. All rights reserved.',
+  }
+
   return (
     <div className={styles.FooterContainer}>
       <div className={styles.FooterContent}>
         <div className={styles.FooterText}>
-          <p>© 2025 Aryan Singh. All rights reserved.</p>
+          <p>{footerContent.copyright}</p>
         </div>
       </div>
     </div>
